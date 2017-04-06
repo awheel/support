a8d454252b97c995b48374f7727add64ff09b22f
fengqi <lyf362345@gmail.com>
2017-04-06 20:33:15 +0800

    添加随机字符串生成

--- src/Str.php
+++ src/Str.php
@@ -9,5 +9,24 @@ namespace awheel\Support;
  */
 class Str
 {
-    // todo
+    /**
+     * 生成一个更真实的“随机”字母数字字符串
+     *
+     * @param int $length
+     *
+     * @return string
+     * @link https://github.com/illuminate/support/blob/master/Str.php#L234
+     */
+    static public function random($length = 16)
+    {
+        $string = '';
+
+        while (($len = strlen($string)) < $length) {
+            $size = $length - $len;
+            $bytes = random_bytes($size);
+            $string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
+        }
+
+        return $string;
+    }
 }
