3969aafc3004064474b7288bf334f79c95bcafe8
fengqi <lyf362345@gmail.com>
2017-04-06 20:32:38 +0800

    添加 size 格式化, 添加 value() 函数

--- src/helpers.php
+++ src/helpers.php
@@ -12,3 +12,32 @@ if (! function_exists('dd')) {
         exit;
     }
 }
+
+if (! function_exists('value')) {
+    /**
+     * 返回给定值的默认值
+     *
+     * @param  mixed  $value
+     * @return mixed
+     * @link https://github.com/laravel/framework/blob/master/src/Illuminate/Support/helpers.php#L916
+     */
+    function value($value)
+    {
+        return $value instanceof Closure ? $value() : $value;
+    }
+}
+
+if (! function_exists('sizeFormat')) {
+    /**
+     * 大小格式化, 传入字位
+     *
+     * @param $bytes
+     * @return string
+     */
+    function sizeFormat($bytes) {
+        $s = ['B', 'Kb', 'MB', 'GB', 'TB', 'PB'];
+        $e = floor(log($bytes)/log(1024));
+
+        return sprintf('%.2f '.$s[$e], ($bytes/pow(1024, floor($e))));
+    }
+}
